b10750f25a27ae03ffe66a9d8aafbbb8506f57de
nyuzyt <jstzzyt01@gmail.com>
2019-06-03 18:13:38 -0400

    some add and modify

--- ns-2.35/queue/hierarchicalQueue.cc
+++ ns-2.35/queue/hierarchicalQueue.cc
@@ -10,64 +10,52 @@ public:
 	}
 } class_hierarchical_queue
 
-void Scheduler::push(Packet packet) {
-    int departureRound = packet.getThryDepartureRound();
-    departureRound = max(departureRound, currentRound);
-    if (departureRound - currentRound >= 100) {
-        packet.setInsertLevel(2);
-        levels[2].push(packet, departureRound / 100);
-    } else if (departureRound - currentRound >= 10) {
-        packet.setInsertLevel(1);
-        levels[1].push(packet, departureRound / 10 % 10);
-    } else {
-        packet.setInsertLevel(0);
-        levels[0].push(packet, departureRound % 10);
+void hierarchicalQueue::enque(Packet* p)
+{
+  hdr_ip* iph = hdr_ip::access(p);
+  
+  // if IPv6 priority = 15 enqueue to queue1
+  if (iph->prio_ == 15) {
+    q1_->enque(p);
+    if ((q1_->length() + q2_->length()) > qlim_) {
+      q1_->remove(p);
+      drop(p);
     }
+  }
+  else {
+    q2_->enque(p);
+    if ((q1_->length() + q2_->length()) > qlim_) {
+      q2_->remove(p);
+      drop(p);
+    }
+  }
 }
 
-Packet Scheduler::serveCycle() {
-    Packet packet = levels[0].pull();
-    if (packet.getThryDepartureRound() == -1) {
-        levels[0].getAndIncrementIndex();
 
-        if (levels[0].getCurrentIndex() == 0) {
-            levels[1].getAndIncrementIndex();
-
-            if (levels[1].getCurrentIndex() == 0)
-                levels[2].getAndIncrementIndex();
-        }
+Packet* hierarchicalQueue::deque()
+{
+  Packet *p;
+  
+  if (deq_turn_ == 1) {
+    p =  q1_->deque();
+    if (p == 0) {
+      p = q2_->deque();
+      deq_turn_ = 1;
     }
-    return packet;
-}
-
-vector<Packet> Scheduler::serveUpperLevel(int& currentCycle, int currentRound) {
-    vector<Packet> result;
-
-    if (!levels[1].isCurrentFifoEmpty()) {
-        int size = static_cast<int>(ceil(levels[1].getCurrentFifoSize() * 1.0 / (10 - levels[1].getCurrentIndex())));
-        for (int i = 0; i < size; i++) {
-            Packet tmp = levels[1].pull();
-            if (tmp.getPacketOrder() == -1)
-                break;
-            currentCycle++;
-            tmp.setDepartureCycle(currentCycle);
-            tmp.setActlDepartureRound(currentRound);
-            result.push_back(tmp);
-        }
+    else {
+      deq_turn_ = 2;
     }
-
-    if (!levels[2].isCurrentFifoEmpty()) {
-        int size = static_cast<int>(ceil(levels[2].getCurrentFifoSize() * 1.0 / (100 - levels[2].getCurrentIndex())));
-        for (int i = 0; i < size; i++) {
-            Packet tmp = levels[2].pull();
-            if (tmp.getPacketOrder() == -1)
-                break;
-            currentCycle++;
-            tmp.setDepartureCycle(currentCycle);
-            tmp.setActlDepartureRound(currentRound);
-            result.push_back(tmp);
-        }
+  }
+  else {
+    p =  q2_->deque();
+    if (p == 0) {
+      p = q1_->deque();
+      deq_turn_ = 2;
     }
-
-    return result;
+    else {
+      deq_turn_ = 1;
+    }
+  }
+  
+  return (p);
 }
