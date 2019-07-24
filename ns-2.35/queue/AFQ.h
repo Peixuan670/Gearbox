e38d9cba3398ad4b391d9667b41711f81df728db
Peixuan670 <peixuan670@outlook.com>
2019-07-24 09:32:11 -0400

    initial update of AFQ

---
+++ ns-2.35/queue/AFQ.h
@@ -0,0 +1,34 @@
+#include "Level.h"
+#include "Flow.h"
+#include <vector>
+
+using namespace std;
+
+class hierarchicalQueue : public Queue {
+private:
+    static const int DEFAULT_VOLUME = 3;
+    int volume;                     // num of levels in scheduler
+    int currentRound;           // current Round
+    int pktCount;           // packet count
+    Level levels[3];
+    Level hundredLevel;
+    Level decadeLevel;
+    vector<Flow> flows;
+    //06262019 Peixuan
+    vector<Packet*> pktCurRound;
+
+    // 06262019 Peixuan
+    vector<Packet*> runRound();
+    vector<Packet*> serveUpperLevel(int);
+    void setPktCount(int);
+public:
+    hierarchicalQueue();
+    explicit hierarchicalQueue(int);
+    void enque(Packet*);
+    Packet* deque();
+    void setCurrentRound(int);
+    int cal_theory_departure_round(hdr_ip*, int);
+    int cal_insert_level(int, int);
+    // Packet* serveCycle();
+    // vector<Packet> serveUpperLevel(int &, int);
+};
