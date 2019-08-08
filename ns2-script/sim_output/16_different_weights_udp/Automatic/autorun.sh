540ab95873104f650cb7fc76df7092fd179fab44
Peixuan670 <peixuan670@outlook.com>
2019-08-08 12:27:09 -0400

    updated test results

--- ns2-script/sim_output/16_different_weights_udp/Automatic/autorun.sh
+++ ns2-script/sim_output/16_different_weights_udp/Automatic/autorun.sh
@@ -2,42 +2,42 @@
 #Put this in the Automatic directory
 
 # HCS
-cp ../c_tcp_1m_and_100k/HCS_out.tr out.tr
+cp ../d_tcp_1m_and_10k/HCS_out.tr out.tr
 bash parse_result.sh
 
 sleep 5
 
-mv ./results/* ../c_tcp_1m_and_100k/HCS/
+mv ./results/* ../d_tcp_1m_and_10k/HCS/
 
 # AFQ10
-cp ../c_tcp_1m_and_100k/AFQ10_out.tr out.tr
+cp ../d_tcp_1m_and_10k/AFQ10_out.tr out.tr
 bash parse_result.sh
 
 sleep 5
 
-mv ./results/* ../c_tcp_1m_and_100k/AFQ10/
+mv ./results/* ../d_tcp_1m_and_10k/AFQ10/
 
 # AFQ100
-cp ../c_tcp_1m_and_100k/AFQ100_out.tr out.tr
+cp ../d_tcp_1m_and_10k/AFQ100_out.tr out.tr
 bash parse_result.sh
 
 sleep 5
 
-mv ./results/* ../c_tcp_1m_and_100k/AFQ100/
+mv ./results/* ../d_tcp_1m_and_10k/AFQ100/
 
 # AFQ1000
-cp ../c_tcp_1m_and_100k/AFQ1000_out.tr out.tr
+cp ../d_tcp_1m_and_10k/AFQ1000_out.tr out.tr
 bash parse_result.sh
 
 sleep 5
 
-mv ./results/* ../c_tcp_1m_and_100k/AFQ1000/
+mv ./results/* ../d_tcp_1m_and_10k/AFQ1000/
 
 # DropTail
-cp ../c_tcp_1m_and_100k/DropTail_out.tr out.tr
+cp ../d_tcp_1m_and_10k/DropTail_out.tr out.tr
 bash parse_result.sh
 
 sleep 5
 
-mv ./results/* ../c_tcp_1m_and_100k/DropTail/
+mv ./results/* ../d_tcp_1m_and_10k/DropTail/
 
